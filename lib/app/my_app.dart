6094c5acc7e211579c8cdf50fb5a99ccc554314d
belal_soliman <belalsoliman8@gmail.com>
2024-10-19 00:56:03 +0300

    create color manger

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class MyApp extends StatefulWidget {
-  MyApp._internal();
-  static final MyApp _instance = MyApp._internal();
+  const MyApp._internal();
+  static const MyApp _instance = MyApp._internal();
   factory MyApp() => _instance;
 
   @override
