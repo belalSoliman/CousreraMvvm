b9d05d9728d3c126486c5ea38d4a83a9094c7925
belal_soliman <belalsoliman8@gmail.com>
2024-10-20 02:05:53 +0300

    route manger  and route function

--- lib/presentaion/resources/routes_manger.dart
+++ lib/presentaion/resources/routes_manger.dart
@@ -1,3 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:mvvmapp/presentaion/forget_password/forget_password_view.dart';
+import 'package:mvvmapp/presentaion/login/log_in.dart';
+import 'package:mvvmapp/presentaion/main/main_view.dart';
+import 'package:mvvmapp/presentaion/register/register_view.dart';
+import 'package:mvvmapp/presentaion/splash/splash_view.dart';
+import 'package:mvvmapp/presentaion/store_details/store_details_view.dart';
+
 class RoutesManger {
   static const String splashRoute = "/";
   static const String logInRoute = "/login";
@@ -5,4 +13,34 @@ class RoutesManger {
   static const String forgetPasswordRoute = "/forgetPassword";
   static const String mainRoute = "/main";
   static const String storeDetailsRoute = "/storeDetail";
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case RoutesManger.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case RoutesManger.logInRoute:
+        return MaterialPageRoute(builder: (_) => const LogInView());
+      case RoutesManger.registerRoute:
+        return MaterialPageRoute(builder: (_) => const RegisterView());
+      case RoutesManger.forgetPasswordRoute:
+        return MaterialPageRoute(builder: (_) => const ForgetPasswordView());
+      case RoutesManger.mainRoute:
+        return MaterialPageRoute(builder: (_) => const MainView());
+      case RoutesManger.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => const StoreDetailsView());
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+        builder: (_) => const Scaffold(
+              body: Center(
+                child: Text("No Route Found"),
+              ),
+            ));
+  }
 }
